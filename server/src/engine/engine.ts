92e0ccc9e33608e70c2dbccd3e1da34780227ca4
Andrew Holligan <2771095h@student.gla.ac.uk>
2024-10-12 14:19:35 +0100

    chore: init myRoom

--- server/src/engine/engine.ts
+++ server/src/engine/engine.ts
@@ -3,15 +3,23 @@ import Matter from "matter-js";
 export class Engine {
 	private engine: Matter.Engine = Matter.Engine.create();
 	private entities: Map<number, Matter.Body> = new Map<number, Matter.Body>();
+	private id: number = 0;
 
-	addEntity(id: number, x: number, y: number, radius: number) {
+	addEntity(x: number, y: number, radius: number): number {
+		this.id++;
 		const entity = Matter.Bodies.circle(x, y, radius);
-		this.entities.set(id, entity);
+		this.entities.set(this.id, entity);
 		Matter.Composite.add(this.engine.world, entity);
+		return this.id;
 	}
 
 	removeEntity(id: number) {
 		const entity = this.entities.get(id);
 		Matter.Composite.remove(this.engine.world, entity);
+	}
+
+	dispose() {
+		Matter.World.clear(this.engine.world, false);
+		Matter.Engine.clear(this.engine);
 	}
 }
--- server/src/rooms/MyRoom.ts
+++ server/src/rooms/MyRoom.ts
@@ -1,21 +1,27 @@
 import { Room, Client } from "@colyseus/core";
 import { GameState } from "./schema/GameState";
+import { Player } from "./schema/Player";
+import { MessageType } from "./schema/enums/MessageType";
+import { Engine } from "../engine/engine";
 
 export class MyRoom extends Room<GameState> {
-	maxClients = 4;
+	public maxClients: number = 10;
+	private engine: Engine = new Engine();
 
-	onCreate(options: any) {
+	onCreate(options: { name: string }) {
 		this.setState(new GameState());
 
-		this.onMessage("type", (client, message) => {
-			//
-			// handle "type" message
-			//
-		});
+		this.onMessage(MessageType.MOVE, (client, message) => {});
+		this.onMessage(MessageType.ROTATE, (client, message) => {});
+		this.onMessage(MessageType.HEAL, (client, message) => {});
+		this.onMessage(MessageType.SHOOT, (client, message) => {});
+		this.onMessage(MessageType.PICKUP, (client, message) => {});
 	}
 
-	onJoin(client: Client, options: any) {
+	onJoin(client: Client, options: { name: string }) {
 		console.log(client.sessionId, "joined!");
+		// TODO - Create a player entity in the engine
+		this.state.entities.set(client.sessionId, new Player());
 	}
 
 	onLeave(client: Client, consented: boolean) {
@@ -24,5 +30,6 @@ export class MyRoom extends Room<GameState> {
 
 	onDispose() {
 		console.log("room", this.roomId, "disposing...");
+		this.engine.dispose();
 	}
 }
