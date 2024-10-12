db470ba1ac05c9396af0dc32f6431b404afc98e9
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-10-12 15:45:12 +0100

    things

--- client/src/game/entity.ts
+++ client/src/game/entity.ts
@@ -1,8 +1,14 @@
 import Vec2 from "./Vec2";
 
 import { EntityType } from "../../../server/src/rooms/schema/enums/EntityType";
+import { Sprite } from "pixi.js";
+import Textures from "./Textures";
+import Player from "./Player";
 
 export default class Entity {
+  private sprite: Sprite | null = null;
+  private spriteType: EntityType | null = null;
+
   constructor(
     public readonly id: number,
     public pos: Vec2,
@@ -10,4 +16,31 @@ export default class Entity {
     public velocity: Vec2,
     public type: EntityType
   ) {}
+
+  update(dt: number) {
+    if (!this.sprite || this.spriteType !== this.type) {
+      this.createSprite();
+    }
+
+    this.pos.x += this.velocity.x * dt;
+    this.pos.y += this.velocity.y * dt;
+
+    this.sprite!.position.set(this.pos.x, this.pos.y);
+    this.sprite!.rotation = this.rotation;
+  }
+
+  private createSprite() {
+    this.sprite = new Sprite();
+    this.sprite.anchor.set(0.5);
+    this.sprite.position.set(this.pos.x, this.pos.y);
+    this.sprite.rotation = this.rotation;
+
+    this.spriteType = this.type;
+
+    if (this.type === EntityType.PLAYER) {
+      this.sprite.texture = Textures.getPlayerTexture((this as any as Player).skin);
+    } else {
+      this.sprite.texture = Textures.getTexture(this.type);
+    }
+  }
 }
--- server/src/rooms/schema/GameState.ts
+++ server/src/rooms/schema/GameState.ts
@@ -1,14 +1,13 @@
-import { MapSchema, Schema, type } from "@colyseus/schema";
+import { ArraySchema, Schema, type } from "@colyseus/schema";
 import { Entity } from "./Entity";
 import { GameStateType } from "./enums/GameStateType";
 import { GameConfig } from "./GameConfig";
 
 export class GameState extends Schema {
-	@type("int64") id: number;
-	@type({ map: Entity }) entities: MapSchema<Entity> =
-		new MapSchema<Entity>();
-	@type("int32") playerCount: number;
-	@type("int32") timeToStart: number;
-	@type("int8") state: GameStateType;
-	@type(GameConfig) config: GameConfig;
+  @type("int64") id: number;
+  @type({ map: Entity }) entities: ArraySchema<Entity> = new ArraySchema<Entity>();
+  @type("int32") playerCount: number;
+  @type("int32") timeToStart: number;
+  @type("int8") state: GameStateType;
+  @type(GameConfig) config: GameConfig;
 }
