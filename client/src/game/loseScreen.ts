29758a1ef03d8b61276a00c077cd9f03b1920f98
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-10-13 03:01:38 +0100

    stop music during video

--- client/src/game/loseScreen.ts
+++ client/src/game/loseScreen.ts
@@ -1,53 +1,58 @@
 import { generatePlacement } from "./generatePlacement";
+import { SoundManager } from "./soundManager";
 
-export default function loseScreen(placement:number) {
+export default function loseScreen(placement: number) {
+  document.body.style.background = "#00000000";
 
-    document.body.style.background = "#00000000"
+  const source = document.createElement("source");
+  source.src = "videos/loseScreen.webm";
+  source.type = "video/webm";
 
-    const source = document.createElement("source");
-    source.src = "videos/loseScreen.webm"
-    source.type = "video/webm"
+  const video = document.createElement("video");
+  video.classList.add("fullscreen");
+  video.classList.add("video-style");
+  video.id = "lose";
+  video.width = 1000;
+  video.height = 1000;
+  video.addEventListener(
+    "play",
+    function () {
+      fallingText();
+    },
+    false
+  );
 
-    const video = document.createElement("video");
-    video.classList.add("fullscreen")
-    video.classList.add("video-style")
-    video.id = "lose"
-    video.width = 1000
-    video.height = 1000
-    video.addEventListener('play', function() {
-        fallingText()
-    }, false);
+  video?.appendChild(source);
 
-    video?.appendChild(source)
+  video.load();
+  video.play();
 
-    video.load()
-    video.play()
-    
-    const overlay = document.getElementsByClassName("overlay")[0];
-    overlay.classList.add("fullscreen")
-    overlay?.appendChild(video)
+  SoundManager.getInstance().stopSound("backgroundMusic");
 
-    function getVideoTime() {
-        return video.currentTime
-    }
+  const overlay = document.getElementsByClassName("overlay")[0];
+  overlay.classList.add("fullscreen");
+  overlay?.appendChild(video);
 
-    function fallingText() {
-        const fallingInterval = setInterval(() => {
-            if (getVideoTime() >= 10.1) {
-                const fallingText = document.createElement("div");
-                fallingText.classList.add("fallingText")
-                fallingText.textContent = "You placed " + generatePlacement(placement)
-        
-                const fallingTextContainer = document.createElement("div");
-                fallingTextContainer.classList.add("fullscreen")
-                fallingTextContainer.classList.add("flexCenterParent")
-                fallingTextContainer.classList.add("fallingTextAnimation")
-                fallingTextContainer.appendChild(fallingText)
-        
-                overlay.appendChild(fallingTextContainer)
-                clearInterval(fallingInterval)
-            }
-        }, 50);
-    }
+  function getVideoTime() {
+    return video.currentTime;
+  }
 
+  function fallingText() {
+    const fallingInterval = setInterval(() => {
+      if (getVideoTime() >= 10.1) {
+        const fallingText = document.createElement("div");
+        fallingText.classList.add("fallingText");
+        fallingText.textContent = "You placed " + generatePlacement(placement);
+
+        const fallingTextContainer = document.createElement("div");
+        fallingTextContainer.classList.add("fullscreen");
+        fallingTextContainer.classList.add("flexCenterParent");
+        fallingTextContainer.classList.add("fallingTextAnimation");
+        fallingTextContainer.appendChild(fallingText);
+
+        overlay.appendChild(fallingTextContainer);
+        clearInterval(fallingInterval);
+      }
+    }, 50);
+  }
 }
